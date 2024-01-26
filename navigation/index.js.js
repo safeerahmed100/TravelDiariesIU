333028d929c5809afc8b9c03c00db4f678db68c4
Ammar123-crypto <zaheerammar5@gmail.com>
2024-01-26 16:04:07 +0500

    Navigation fix

---
+++ navigation/index.js.js
@@ -0,0 +1,35 @@
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import * as React from 'react'
+import WelcomeScreen from '../screens/WelcomeScreen'
+import LoginScreen from '../screens/LoginScreen'
+import SignupScreen from '../screens/SignupScreen'
+import ProfileSetup from '../screens/ProfileSetup'
+import HomeScreen from '../screens/HomeScreen'
+import Journal from '../screens/Journal'
+import CreateJournal from '../screens/CreateJournal'
+import MapScreen from '../screens/MapScreen'
+import PolicyScreen from '../screens/PolicyScreen'
+
+const Stack = createNativeStackNavigator();
+
+function AppNavigation() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName='Welcome' screenOptions={{ headerShown: false }}>
+            <Stack.Screen name='Login' component={LoginScreen} />
+            <Stack.Screen name='Signup' component={SignupScreen} />
+            <Stack.Screen name='Profile' component={ProfileSetup} />
+            <Stack.Screen name='Home' component={HomeScreen} />
+            <Stack.Screen name='Journal' component={Journal} />
+            <Stack.Screen name='CreateJournal' component={CreateJournal} />
+            <Stack.Screen name='Map' component={MapScreen} />
+            <Stack.Screen name='Policy' component={PolicyScreen} />
+
+            <Stack.Screen name='Welcome' component={WelcomeScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
+
+export default AppNavigation;
